1636f89edad6cd82f11ef8d8b5c251764db0fd6f
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-12-12 17:08:35 +0100

    aggiunta prodotti

--- index.php
+++ index.php
@@ -13,6 +13,33 @@ require __DIR__ . '/models/Toy.php';
 require __DIR__ . '/models/Accessory.php';
 
 
-$cuccia = new Accessory(new Category('Cuccia per cani'))
+$products = [
+  new Food(new Category('Cat', '<i class="fa-solid fa-cat"></i>'), 'whiskas pranzetti', 'whiskas', 10, true, 'https://m.media-amazon.com/images/I/71CSgd7hkML._AC_SY450_PIbundle-12,TopRight,0,0_SH20_.jpg', 'vegetables', 2, 'adult'),
 
-var_dump($cuccia);
+  new Toy(new Category('Dog', '<i class="fa-solid fa-dog"></i>'), 'Osso giocattolo', 'pet & pet', 5, false, 'https://m.media-amazon.com/images/I/71Of7YEuSJL._AC_SY355_.jpg', '20cm', 'orange', 'plastic'),
+
+  new Accessory(new Category('Dog', '<i class="fa-solid fa-dog"></i>'), 'Guinzaglio', 'Alldog', 20, true, 'https://www.toscani.store/wp-content/uploads/2020/08/guinzaglio-cane-150cm-nero-dark-toscani-store.jpeg', true)
+
+];
+
+var_dump($products);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
